ec516f14dbd7b68c54d01aadf268d223f6582ecd
swartsd <swa17001@byui.edu>
2023-07-12 14:00:49 -0600

    update test_user.py

--- tests/test_user.py
+++ tests/test_user.py
@@ -3,33 +3,29 @@ import requests
 import requests_mock
 
 @pytest.fixture
-def api_url():
-    return "http://127.0.0.1:8000/users"
+def endpoint():
+    return 'http://127.0.0.1:8000/users'
 
-def test_empty_response_with_401(api_url, mocker):
+def test_empty_response_and_401(endpoint, mocker):
     params = {
-        "username": "admin",
-        "password": "admin"
+        'username': 'admin',
+        'password': 'admin'
     }
 
-    with requests_mock.Mocker() as mock_request:
-        mock_request.get(api_url, status_code=401, text='')
+    with requests_mock.Mocker() as mock:
+        mock.get(endpoint, status_code=401, text='')
+        response = requests.get(endpoint, params=params)
+        assert response.status_code == 401
+        assert response.text == ''
 
-        response = requests.get(api_url, params=params)
-    
-    assert response.status_code == 401
-    assert response.text == ''
-
-def test_empty_response_with_200(api_url, mocker):
+def test_empty_response_and_200(endpoint, mocker):
     params = {
-        "username": "admin",
-        "password": "qwerty"
+        'username': 'admin',
+        'password': 'qwerty'
     }
 
-    with requests_mock.Mocker() as mock_request:
-        mock_request.get(api_url, status_code=200, text='')
-
-        response = requests.get(api_url, params=params)
-    
-    assert response.status_code == 200
-    assert response.text == ''
+    with requests_mock.Mocker() as mock:
+        mock.get(endpoint, status_code=200, text='')
+        response = requests.get(endpoint, params=params)
+        assert response.status_code == 200
+        assert response.text == ''
